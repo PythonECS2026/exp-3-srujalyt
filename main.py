83bbf7084fd60cad64946152e165765649ba4d66
srujalyt <srujalhawelikar07@gmail.com>
2026-02-19 21:04:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,17 @@
 # Coder:
 # Date:
 
-# Write your code here
+basic_salary = float(input("Enter the Basic Salary (BS): "))
+da = 0.70 * basic_salary
+ta = 0.30 * basic_salary
+hra = 0.10 * basic_salary
+gross_salary = basic_salary + da + ta + hra
+
+
+print(f"Basic Salary:    {basic_salary}\t")
+print(f"Dearness (DA):   {da}\t")
+print(f"Travel (TA):     {ta}\t")
+print(f"House Rent(HRA): {hra}\t")
+
+print(f"GROSS SALARY:    {gross_salary}")
+
