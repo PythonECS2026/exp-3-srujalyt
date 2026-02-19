3c4166ed851d34652d1ca83a95b7301ea0732191
srujalyt <srujalhawelikar07@gmail.com>
2026-02-19 21:47:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 # AIM: Write a Python program to calculate the gross salary of an employee.
 # Coder:Srujal
 # Date:18/02/2026
-basic_salary = float(input("Enter the Basic Salary (BS): "))
-da = 0.70 * basic_salary
-ta = 0.30 * basic_salary
-hra = 0.10 * basic_salary
-gross_salary = basic_salary + da + ta + hra
-print(f"Basic Salary:    {basic_salary}\t")
-print(f"Dearness (DA):   {da}\t")
-print(f"Travel (TA):     {ta}\t")
-print(f"House Rent(HRA): {hra}\t")
 
-print(f"GROSS SALARY:    {gross_salary}")
+print("Salary Calculator")
+Basic_Salary = float(input("Enter the Basic Salary(BA) : "))
+
+print("Salary Details:")
+DA = 0.70 * Basic_Salary
+TA = 0.30 * Basic_Salary
+HRA = 0.10 * Basic_Salary
+gross_salary = Basic_Salary + DA + TA + HRA
+print(f"Basic Salary:  {Basic_Salary}\t")
+print(f"DA(70%):       {DA}\t")
+print(f"TA(30%):       {TA}\t")
+print(f"HRA(10%):      {HRA}\t")
+print(f"GROSS SALARY:  {gross_salary}")
 
